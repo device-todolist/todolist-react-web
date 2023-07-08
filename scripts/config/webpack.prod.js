9ddb7a032645c91bd2b9596f5d3686675b2fdfad
chenxingyu0830 <chenxingyu0830@gmail.com>
2023-07-09 04:43:48 +0800

    build: add webpack-bundle-analyzer

--- scripts/config/webpack.prod.js
+++ scripts/config/webpack.prod.js
@@ -1,6 +1,7 @@
 const Webpack = require("webpack");
 const { merge } = require("webpack-merge");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const TerserPlugin = require("terser-webpack-plugin");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
@@ -14,6 +15,11 @@ module.exports = merge(common, {
       filename: "css/[name].[contenthash:8].css",
       chunkFilename: "css/[name].[contenthash:8].chunk.css",
       ignoreOrder: false,
+    }),
+    new BundleAnalyzerPlugin({
+      analyzerMode: "server", // 开一个本地服务查看报告
+      analyzerHost: "127.0.0.1", // host 设置
+      analyzerPort: 8888, // 端口号设置
     }),
     new Webpack.BannerPlugin({
       raw: true,
